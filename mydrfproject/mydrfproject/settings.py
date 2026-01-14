45140881eaa9bc6597c8eecca6a3fd1b75884bbc
md-ajim <mdajim2669@gmail.com>
2026-01-14 17:06:51 +0600

    Switch database backend from SQLite to MySQL.
    Replaced the default SQLite configuration with MySQL settings in `DATABASES`. The old SQLite configuration has been commented out for reference. This change prepares the project for production with a more robust database solution.

--- mydrfproject/mydrfproject/settings.py
+++ mydrfproject/mydrfproject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
 
 # Quick-start development settings - unsuitable for production
@@ -88,9 +89,11 @@ WSGI_APPLICATION = "mydrfproject.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default':{
+      'ENGINE' : 'django.db.backends.mysql',
+       'NAME' :  'mydb',
+       'USER' : 'root',
+       'PASSWORD':        
     }
 }
 
@@ -135,3 +138,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
