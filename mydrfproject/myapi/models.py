e51b0f51175c8bd6e30d8dbd4a8ebbeba59f4f6f
m-ajim <mdajim2669@gmail.com>
2026-03-26 23:11:44 +0600

    Add import for Django's User model
    This change adds an import for the `User` model from Django's `contrib.auth.models`. This is likely in preparation for integrating user-related functionality in the `models.py` file.

--- mydrfproject/myapi/models.py
+++ mydrfproject/myapi/models.py
@@ -11,3 +11,12 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
