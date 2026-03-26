9edca019bb9610926f8e5b19803dd6f934638c05
m-ajim <mdajim2669@gmail.com>
2026-03-26 23:15:10 +0600

    Add OrderItem model to represent items in an order
    The new OrderItem model links Order and Product models and includes a quantity field. This change enables better management of products within individual orders.

--- mydrfproject/myapi/models.py
+++ mydrfproject/myapi/models.py
@@ -25,3 +25,10 @@ class Product(models.Model):
  class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)       
+
+
+
+ class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)   
